dc659ded70b18e3325c538df50509a3629aeb196
HungNQ <hunghhll123@gmail.com>
2025-11-05 17:01:58 +0700

    add profile page

--- frontend/src/pages/UserProfilePage/index.jsx
+++
@@ -1,8 +0,0 @@
-import UserImage from "../../components/UserImage";
-import avatarPlaceholder from '../../assests/images/avatar-placeholder.jpg'
-
-export default function UserProfilePage() {
-    return <div>
-        <UserImage size={'lg'} imgUrl={avatarPlaceholder} />
-    </div>
-}
